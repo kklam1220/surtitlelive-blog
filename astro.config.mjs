a16c919f81fa6e8816fa352c625d855122ef4886
kklam1220 <kklam1220@gmail.com>
2025-12-31 20:40:34 -0700

    SEO: Update site base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.surtitlelive.com',
+	site: 'https://surtitlelive.com',
+	base: '/blog',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 	// Cloudflare Pages adapter (commented out for static builds, uncomment if using SSR)
